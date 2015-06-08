eddeab8651fffbc5f3bc0fa0a513a4a0029d6574
unknown <Tom.Carter@UKMM-SC-365.QUANTUM.LOCAL>
2015-06-08 11:05:49 +0100

    Unprotected added

---
+++ CampervibeSso.TestResource.WebApi/Controllers/UnprotectedController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Security.Claims;
+using System.Web.Http;
+
+namespace CampervibeSso.TestResource.WebApi.Controllers
+{
+	[Authorize]
+	[RoutePrefix("api/unprotected")]
+    public class UnprotectedController : ApiController
+	{
+		[Route("")]
+		public IEnumerable<object> Get()
+		{
+			var identity = User.Identity as ClaimsIdentity;
+
+			return identity.Claims.Select(c => new
+			{
+				Type = c.Type,
+				Value = c.Value
+			});
+		}
+    }
+}
